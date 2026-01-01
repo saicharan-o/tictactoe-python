60df3a55bebfb30351b17d7052be8adc3c30b60d
saicharan-o <saicharanr227@gmail.com>
2026-01-01 22:33:46 +0530

    Days

--- plot.py
+++ plot.py
@@ -1,7 +1,7 @@
 import matplotlib.pylab as plt
 a=[1,3,5,7,9,11,13,15,18]
 b=[39,50,48,61,37,49,56,29,30]
-plt.xlabel("Days")
+plt.xlabel("Days") 
 plt.ylabel("Tempuratur")
 plt.title("Data")
 plt.plot(a,b,color="black",linewidth=2)
