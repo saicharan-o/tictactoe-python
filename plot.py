d162ebbb0b1858d16383fe3ed2e92c33b7578df4
saicharan-o <saicharanr227@gmail.com>
2026-01-01 22:30:12 +0530

    Days

--- plot.py
+++ plot.py
@@ -1,8 +1,8 @@
 import matplotlib.pylab as plt
 a=[1,3,5,7,9,11,13,15,18]
-b=[0,97,8,10,27,90,76,2,20]
-plt.xlabel("Ratings")
-plt.ylabel("UnderRatings")
+b=[39,50,48,61,37,49,56,29,30]
+plt.xlabel("Days")
+plt.ylabel("Tempuratur")
 plt.title("Data")
 plt.plot(a,b,color="black",linewidth=2)
 plt.show()
