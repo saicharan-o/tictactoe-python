3bf6128b008aedb2ae731e8c3d072d6b43192300
saicharan-o <saicharanr227@gmail.com>
2026-01-02 17:51:47 +0530

    Mark

--- pie.py
+++
@@ -1,8 +0,0 @@
-import matplotlib.pylab as plt
-b=[1190,810,528,691,1009,889,416,123,70]
-a=["Electronics","Clothing","Groceries","Home Appliances","Furniture","Footwear","Books","Toys","Others"]
-plt.title("Category-wise Distribution of Values")
-plt.axis("equal")
-plt.pie(b,labels=a,autopct="%0.1f%%",radius=1.06)
-plt.savefig(r"C:\Users\saich\OneDrive\Desktop\python\tictactoe\piechart.png",bbox_inches="tight",pad_inches=4,transparent=True)
-plt.show()
